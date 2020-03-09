88d3bf621dc9e0623b834903aad8f546f4e24a1e
Tim Whitaker <camp4climber@gmail.com>
2020-03-08 19:09:12 -0600

    Start gym implementation

---
+++ doublepole/doublepole/envs/doublepole_env.py
@@ -0,0 +1,15 @@
+import gym
+
+class CartPole(gym.Env):
+    metadata = {'render.modes': ['human']}   
+    def __init__(self):
+        pass
+ 
+    def step(self, action):
+        pass
+ 
+    def reset(self):
+        pass
+ 
+    def render(self, mode='human', close=False):
+        pass
