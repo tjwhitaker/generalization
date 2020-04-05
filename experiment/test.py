23c7b42315ceacc8cffd34a9c6d4b367a5772476
Tim Whitaker <camp4climber@gmail.com>
2020-04-05 16:59:37 -0600

    Add gen tests

--- experiment/test.py
+++ experiment/test.py
@@ -1,5 +1,7 @@
 from spinup.utils.run_utils import ExperimentGrid
+from spinup.utils.test_policy import load_policy_and_env
 from spinup import vpg_tf1, trpo_tf1, ppo_tf1, ddpg_tf1, td3_tf1, sac_tf1
+import numpy as np
 import tensorflow as tf
 import gym
 
@@ -15,7 +17,7 @@ def train(args):
         eg.add('env_fn', env_fn)
         eg.add('epochs', args.epochs)
         eg.add('seed', [10*i for i in range(args.num_runs)])
-        eg.add('ac_kwargs:hidden_sizes', (16,16), 'hid')
+        eg.add('ac_kwargs:hidden_sizes', (32,32), 'hid')
         eg.add('ac_kwargs:activation', tf.nn.relu)
         eg.run(algo, num_cpu=args.cpu)
 
@@ -31,52 +33,53 @@ def test():
 
     ranges = [4.28, 2.70, 17.2, 7.2]
     percentages = [0.05, 0.25, 0.50, 0.75, 0.95]
+    models = ["/home/tim/Research/generalization/spinningup/data/vpg/vpg_s0/",
+              "/home/tim/Research/generalization/spinningup/data/trpo/trpo_s0/",
+              "/home/tim/Research/generalization/spinningup/data/ppo/ppo_s0/",
+              "/home/tim/Research/generalization/spinningup/data/ddpg/ddpg_s0/",
+              "/home/tim/Research/generalization/spinningup/data/td3/td3_s0/",
+              "/home/tim/Research/generalization/spinningup/data/sac/sac_s0/"]
 
-    env = env_fn()
+    for model in models:
+        score = 0
 
-    score = 0
+        env, get_action = load_policy_and_env(model, "last", False)
 
-    for s0 in percentages:
-        for s1 in percentages:
-            for s2 in percentages:
-                for s3 in percentages:
-                    state = [0, 0, 0, 0, 0, 0]
-                    state[0] = (s0 * ranges[0] - ranges[0] / 2)
-                    state[1] = (s1 * ranges[1] - ranges[1] / 2)
-                    state[2] = (s2 * ranges[2] - ranges[2] / 2) * 2 * np.pi / 360
-                    state[3] = (s3 * ranges[3] - ranges[3] / 2) * 2 * np.pi / 360
+        for s0 in percentages:
+            for s1 in percentages:
+                for s2 in percentages:
+                    for s3 in percentages:
+                        state = [0, 0, 0, 0, 0, 0]
+                        state[0] = (s0 * ranges[0] - ranges[0] / 2)
+                        state[1] = (s1 * ranges[1] - ranges[1] / 2)
+                        state[2] = (s2 * ranges[2] - ranges[2] / 2) * 2 * np.pi / 360
+                        state[3] = (s3 * ranges[3] - ranges[3] / 2) * 2 * np.pi / 360
 
-                    # Run test(policy)
-                    success_flag = True
-                    observation = env.reset_to_state(state)
-                    for i in range(1000):
-                        env.render()
-                        #action = model output (observation)
-                        observation, reward, done, info = env.step(action)
+                        # Run test(policy)
+                        success_flag = True
+                        observation = env.reset_to_state(state)
+                        for i in range(1000):
+                            # env.render()
+                            action = get_action(observation)
+                            observation, reward, done, info = env.step(action)
+                            
+                            if done:
+                                success_flag = False
+                                break
                         
-                        if done:
-                            success_flag = False
-                            break
-                    
-                    if success_flag:
-                        score = score + 1
+                        if success_flag:
+                            score = score + 1
     
-    print(score)
-    return score
-                        
-
-
-
+        print(model, score)
+        tf.reset_default_graph()
 
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument('--cpu', type=int, default=4)
     parser.add_argument('--num_runs', type=int, default=1)
-    parser.add_argument('--epochs', type=int, default=200)
+    parser.add_argument('--epochs', type=int, default=2000)
     args = parser.parse_args()
 
     train(args)
-    test()
-
-    
+    # test()
