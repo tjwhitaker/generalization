04e56734c906c65c64772f53e1428630480d202c
Tim Whitaker <camp4climber@gmail.com>
2020-05-18 18:39:50 -0600

    Add gym registration

--- src/environments/singlepole/__init__.py
+++ src/environments/singlepole/__init__.py
@@ -1,1 +1,24 @@
+import gym
 from .singlepole_env import SinglePoleEnv
+from gym.envs.registration import register
+
+
+# Prevent Errors with Re-Registrations on Import
+def register_env(id, entry_point, force=True):
+    env_specs = gym.envs.registry.env_specs
+    if id in env_specs.keys():
+        if not force:
+            return
+        del env_specs[id]
+    register(
+        id=id,
+        entry_point=entry_point,
+        max_episode_steps=5000,
+        reward_threshold=4950.0,
+    )
+
+
+register_env(
+    id='SinglePole-v0',
+    entry_point='src.environments.singlepole:SinglePoleEnv'
+)
